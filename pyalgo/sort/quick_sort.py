3ec07c8215e23dfec79827b4c0694560778cf833
Hu Zheng <axurez@vip.qq.com>
2016-01-12 20:18:54 +0800

    Add return type check.

--- pyalgo/sort/quick_sort.py
+++ pyalgo/sort/quick_sort.py
@@ -8,14 +8,14 @@ def p_decorate(func):
 
         arg_count = func.__code__.co_argcount
         arg_name_list = func.__code__.co_varnames[:arg_count]
-        arg_name_with_type_list = typing.get_type_hints(func)
-        func_args_with_type = map(lambda x: ( (x, arg_name_with_type_list[x]) ), arg_name_list)
+        arg_name_with_type_dict = typing.get_type_hints(func)
+        func_args_with_type = map(lambda x: ( (x, arg_name_with_type_dict[x]) ), arg_name_list)
         func_args_with_type_and_value = zip(func_args_with_type, args)
 
         ### Type checking
         for index in range(arg_count):
             print(arg_name_list[index])
-            expected_type = arg_name_with_type_list[arg_name_list[index]]
+            expected_type = arg_name_with_type_dict[arg_name_list[index]]
             if not isinstance(args[index], expected_type):
                 raise TypeError("Type mismatch! Expected: '" + expected_type.__name__ + "'. Given: '" + type(args[index]).__name__ + "'.")
 
@@ -30,18 +30,26 @@ def p_decorate(func):
         for arg in args:
             print(arg)
         # return "<p>{0}</p>".format(func(args))
-        return func(*args, **kwargs)
+        result = func(*args, **kwargs)
+        if 'return' in arg_name_with_type_dict:
+            expected_type = arg_name_with_type_dict['return']
+            if not isinstance(result, expected_type):
+                raise TypeError("Return type mismatch! Designated " + expected_type.__name__ + ", got " + type(result).__name__)
+        else:
+            if result is not None:
+                raise TypeError("Return type mismatch! Designated None, got " + type(result).__name__)
+        return result
     return func_wrapper
 
 @p_decorate
-def quick_sort(a: list):
+def quick_sort(a: list) -> list:
     j = 3
     for i in a:
         print(i)
     return a
 
 @p_decorate
-def median_of_three(za: list, l: int, r: int) -> int:
+def median_of_three(za: list, l: int, r: int) -> list:
     print(za, l, r)
     return za
 
@@ -50,7 +58,7 @@ def median_of_three(za: list, l: int, r: int) -> int:
 if __name__ == '__main__':
     # print(3)
     # quick_sort([3, 4, 5])
-    median_of_three(3, 4, 5)
+    median_of_three([3, 4], 4, 5)
 
     print(quick_sort([3, 4, 5]))
     # print(typing.get_type_hints(quick_sort))
